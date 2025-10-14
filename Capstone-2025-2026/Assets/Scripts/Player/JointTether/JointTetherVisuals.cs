2b94854694bbc96e2c58efdcd1fcd527ada52c8e
Drarist76 <deegankosen@gmail.com>
2025-10-14 17:56:31 -0400

    Added bend to tether. When tether activates, tether straightens up and becomes thinner

--- Capstone-2025-2026/Assets/Scripts/Player/JointTether/JointTetherVisuals.cs
+++ Capstone-2025-2026/Assets/Scripts/Player/JointTether/JointTetherVisuals.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.Mathematics;
 using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
@@ -6,8 +8,9 @@ public class JointTetherVisuals : MonoBehaviour
     private LineRenderer _lineRenderer;
 
     [Header("Line Variables")]
-    public float lineSegmentSize = 0.15f;
-    public float lineWidth = 0.1f;
+    [SerializeField] private float lineSegmentSize = 0.15f;
+    [SerializeField] private float lineWidth = 0.1f;
+    [SerializeField] private float bendAmount = 0.5f;
     [SerializeField] Color activatedStateColor = Color.green;
     [SerializeField] Color inactiveStateColor = Color.yellow;
     [SerializeField] Color stretchedStateColor = Color.red;
@@ -17,20 +20,23 @@ public class JointTetherVisuals : MonoBehaviour
     private Vector3 endLocalPosition;
 
     private Vector3 middlePosition;
-    private Vector3 startMiddelPosition;
-    private Vector3 endMiddelPosition;
+    private Vector3 startMiddlePosition;
+    private Vector3 endMiddlePosition;
+    private float bendMult = 1f;
+    private float timeToStraighenLine;
 
     //PRIVATE
-    private CurvedLinePoint[] linePoints = new CurvedLinePoint[0];
+    private Vector3[] linePoints = new Vector3[5];
     private Vector3[] linePositions = new Vector3[0];
     private Vector3[] linePositionsOld = new Vector3[0];
 
-    public void Init(Transform startTransform, Vector3 startLocalPosition, Transform endTransform, Vector3 endLocalPosition, bool startsActive)
+    public void Init(Transform startTransform, Vector3 startLocalPosition, Transform endTransform, Vector3 endLocalPosition, bool startsActive, float activationDelay)
     {
         this.startTransform = startTransform;
         this.endTransform = endTransform;
         this.startLocalPosition = startLocalPosition;
         this.endLocalPosition = endLocalPosition;
+        timeToStraighenLine = activationDelay;
 
         if(startsActive) SetLineColorActive();
         else SetLineColorInactive();
@@ -44,14 +50,19 @@ public class JointTetherVisuals : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _lineRenderer.SetPosition(0, startTransform.TransformPoint(startLocalPosition));
-        _lineRenderer.SetPosition(1, endTransform.TransformPoint(endLocalPosition));
+        //_lineRenderer.SetPosition(0, startTransform.TransformPoint(startLocalPosition));
+        //_lineRenderer.SetPosition(1, endTransform.TransformPoint(endLocalPosition));
+
+        UpdateMiddlePointPosition();
+        GetPoints();
+        SetPointsToLine();
     }
 
     public void SetLineColorActive()
     {
         _lineRenderer.startColor = activatedStateColor;
         _lineRenderer.endColor = activatedStateColor;
+        StartCoroutine(MakeLineStraight());
     }
 
     public void SetLineColorInactive()
@@ -60,22 +71,37 @@ public class JointTetherVisuals : MonoBehaviour
         _lineRenderer.endColor = inactiveStateColor;
     }
 
-    void GetPoints()
+    private void UpdateMiddlePointPosition()
     {
-        Debug.Log("Line");
+        Vector3 startWorldPos = startTransform.TransformPoint(startLocalPosition);
+        Vector3 endWorldPos = endTransform.TransformPoint(endLocalPosition);
+        Vector3 startToEndVector = endWorldPos - startWorldPos;
+
+        middlePosition = startWorldPos + startToEndVector/2 + Vector3.down * bendAmount * bendMult;
+        startMiddlePosition = startWorldPos + startToEndVector / 4 + Vector3.down * bendAmount * 2/3 * bendMult;
+        endMiddlePosition = startWorldPos + startToEndVector * 3/4 + Vector3.down * bendAmount * 2 / 3 * bendMult;
 
         //find curved points in children
-        linePoints = this.GetComponentsInChildren<CurvedLinePoint>();
+        linePoints[0] = startWorldPos;
+        linePoints[1] = startMiddlePosition;
+        linePoints[2] = middlePosition;
+        linePoints[3] = endMiddlePosition;
+        linePoints[4] = endWorldPos;
+    }
+
+    private void GetPoints()
+    {
+        Debug.Log("Line");
 
         //add positions
         linePositions = new Vector3[linePoints.Length];
         for (int i = 0; i < linePoints.Length; i++)
         {
-            linePositions[i] = linePoints[i].transform.position;
+            linePositions[i] = linePoints[i];
         }
     }
 
-    void SetPointsToLine()
+    private void SetPointsToLine()
     {
         //create old positions if they dont match
         if (linePositionsOld.Length != linePositions.Length)
@@ -108,5 +134,21 @@ public class JointTetherVisuals : MonoBehaviour
             line.startWidth = lineWidth;
             line.endWidth = lineWidth;
         }
+    }
+
+    IEnumerator MakeLineStraight()
+    {
+        float timer = timeToStraighenLine;
+        while(timer > 0)
+        {
+            timer -= Time.deltaTime;
+            bendMult = Mathf.Pow(timer / timeToStraighenLine, 2);
+            yield return null;
+        }
+
+        LineRenderer line = this.GetComponent<LineRenderer>();
+        line.startWidth = lineWidth * 0.3f;
+        line.endWidth = lineWidth * 0.3f;
+        lineWidth *= 0.3f;
     }
 }
