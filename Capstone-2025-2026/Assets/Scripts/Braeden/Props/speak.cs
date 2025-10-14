73bf3da6d34ff407c405aab8eefa5e64d8ed7f10
eloravw <67245265+eloravw@users.noreply.github.com>
2025-10-14 13:24:21 -0400

    NPC pop-up dialogue
    Added billboarding, now handles 2D sprite. Added in-range collider

--- Capstone-2025-2026/Assets/Scripts/Braeden/Props/speak.cs
+++ Capstone-2025-2026/Assets/Scripts/Braeden/Props/speak.cs
@@ -1,23 +1,38 @@
+using NodeCanvas.DialogueTrees;
 using UnityEngine;
 
 public class speak : MonoBehaviour
 {
-    public GameObject dialouge;
+    public GameObject dialogue;
 
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            dialouge.SetActive(true);
+            dialogue.SetActive(true);
         }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+
+            Vector3 target = other.transform.position - transform.position;
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, target, 15f, 0.0f);
+            transform.rotation = Quaternion.LookRotation(newDirection);
+
+        }
+
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            dialouge.SetActive(false);
+            dialogue.SetActive(false);
         }
     }
 }
