4f6b59b967d2dc4dffd0c6aeaa2920906d6d04b9
Jonathan To <jonto398@gmail.com>
2025-10-15 02:04:44 -0400

    feat: aim assist cleanup + buffer refinements

--- Capstone-2025-2026/Assets/Scripts/Player/CoreMechanics/Lasso/AimAssist.cs
+++ Capstone-2025-2026/Assets/Scripts/Player/CoreMechanics/Lasso/AimAssist.cs
@@ -10,7 +10,8 @@ public enum AimAssistType
 public class AimAssist
 {
     private Prop _currentlyHighlightedProp;
-    LayerMask tetherLayerIgnore = ~(1 << LayerMask.NameToLayer("Tether"));
+    private LayerMask tetherLayerIgnore = ~(1 << LayerMask.NameToLayer("Tether"));
+
     #region Closest Target (for snap)
     public Prop GetClosestTarget(Camera cam, Vector3 origin, float range)
     {
@@ -54,75 +55,122 @@ public class AimAssist
     #region Main Aim Assist Function
     public RaycastHit? GetAssistHitPoint(Camera cam, Vector3 origin, float range, AimAssistType type, float bufferRadius) //the ? means it can return null
     {
+        Ray directHitRay = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
         switch (type)
         {
             case AimAssistType.Snap:
-                Prop target = GetClosestTarget(cam, origin, range); //get closest target to middle of screen, already has prop check
-                if (target != null)
-                {
-                    Vector3 targetPos = target.transform.position;
-                    Ray snapRay = new Ray(cam.transform.position, (targetPos - cam.transform.position).normalized);
-                    if (Physics.Raycast(snapRay, out RaycastHit snapHit, range, tetherLayerIgnore))
-                    {
-                        return snapHit;
-                    }
-                }
+                if (GetSnapHit(cam, origin, range, out RaycastHit snapHit)) 
+                    return snapHit;
                 break;
 
             case AimAssistType.Buffer:
-                Ray bufferRay = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
-                if (Physics.Raycast(bufferRay, out RaycastHit bufferHit, range, tetherLayerIgnore)) //raycast first
-                {
-                    if (bufferHit.transform.GetComponentInParent<Prop>() != null)
-                    {
-                        return bufferHit;
-                    }
-                }
-
-                //sweep spherecast (spherecast just hits the first thing)
-                RaycastHit[] hits = Physics.SphereCastAll(cam.transform.position, bufferRadius, bufferRay.direction, range, tetherLayerIgnore);
-                if (hits.Length > 0)
-                {
-                    RaycastHit bestTargetHit = new RaycastHit();
-                    float closestSqrDistance = float.MaxValue;
-                    bool foundProp = false;
-
-                    foreach (RaycastHit hit in hits)
-                    {
-                        //ignore if no prop
-                        if (hit.transform.GetComponentInParent<Prop>() == null) continue;
-
-                        //check if this is the closest option
-                        float sqrDistance = (cam.transform.position - hit.transform.position).sqrMagnitude;
-                        if (sqrDistance < closestSqrDistance)
-                        {
-                            closestSqrDistance = sqrDistance;
-                            bestTargetHit = hit;
-                            foundProp = true;
-                        }
-                    }
-
-                    if (foundProp)
-                    {
-                        return bestTargetHit;
-                    }
-                }
+                if (GetDirectHit(directHitRay, range, out RaycastHit directHit))
+                    return directHit;
+                if (GetDynamicBufferHit(cam, directHitRay, range, 0f, bufferRadius, out RaycastHit bufferHit))
+                    return bufferHit;
                 break;
 
             case AimAssistType.None:
             default:
-                Ray noAssistRay = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
-                if (Physics.Raycast(noAssistRay, out RaycastHit noAssistHit, range, tetherLayerIgnore)) //original functionality
-                {
-                    if (noAssistHit.transform.GetComponentInParent<Prop>() != null)
-                    {
-                        return noAssistHit;
-                    }
-                }
+                if (GetDirectHit(directHitRay, range, out directHit))
+                    return directHit;
                 break;
         }
         return null;
     }
+    #endregion
+
+    #region Snap
+
+    private bool GetSnapHit(Camera cam, Vector3 origin, float range, out RaycastHit hit)
+    {
+        hit = new RaycastHit();
+        Prop target = GetClosestTarget(cam, origin, range); //get closest target to middle of screen, already has prop check
+        if (target != null)
+        {
+            Vector3 targetPos = target.transform.position;
+            Ray snapRay = new Ray(cam.transform.position, (targetPos - cam.transform.position).normalized);
+            if (Physics.Raycast(snapRay, out RaycastHit snapHit, range))
+            {
+                hit = snapHit;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    #endregion
+
+    #region Direct Hit
+
+    private bool GetDirectHit(Ray ray, float range, out RaycastHit hit)
+    {
+        hit = new RaycastHit();
+        if (Physics.Raycast(ray, out RaycastHit bufferHit, range, tetherLayerIgnore))
+        {
+            if (bufferHit.transform.GetComponentInParent<Prop>() != null)
+            {
+                hit = bufferHit;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    #endregion
+
+    #region Buffer Hit
+
+    private bool GetDynamicBufferHit(Camera cam, Ray ray, float range, float minRadius, float maxRadius, out RaycastHit bestHit)
+    {
+        bestHit = new RaycastHit();
+        bool foundProp = false;
+        float closestSqrDistance = float.MaxValue;
+
+        int steps = 10; //number of slices along the ray (i.e. number of times it increases in radius)
+        Vector3 origin = ray.origin;
+
+        for (int i = 1; i <= steps; i++)
+        {
+            //% along the 'cone'
+            float startPercent = (float)i / steps;
+            float endPercent = (float)(i + 1) / steps;
+
+            //start and end points of current segment
+            float startDistance = startPercent * range;
+            float endDistance = endPercent * range;
+            float segmentRadius = Mathf.Lerp(minRadius, maxRadius, endPercent);
+
+            //length + origin of segment for spherecast
+            float segmentLength = endDistance - startDistance;
+            Vector3 segmentOrigin = ray.origin + ray.direction * startDistance;
+
+            //only spherecast the current segment for 'growing cone' effect
+            RaycastHit[] hits = Physics.SphereCastAll(segmentOrigin, segmentRadius, ray.direction, segmentLength, tetherLayerIgnore);
+            foreach (RaycastHit hit in hits)
+            {
+                if (hit.transform.GetComponentInParent<Prop>() == null)
+                    continue;
+
+                float sqrDistance = (origin - hit.point).sqrMagnitude;
+                if (sqrDistance < closestSqrDistance)
+                {
+                    closestSqrDistance = sqrDistance;
+                    bestHit = hit;
+                    foundProp = true;
+                }
+            }
+
+            if (foundProp)
+                break;
+        }
+
+        return foundProp;
+    }
+
+
+
     #endregion
 
     #region Highlight
