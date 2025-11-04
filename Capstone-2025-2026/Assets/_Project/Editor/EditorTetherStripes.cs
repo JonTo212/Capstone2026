d518efac3396e25b486e7ce28e3fe402e55e3737
Drarist76 <deegankosen@gmail.com>
2025-11-03 20:03:56 -0500

    added stripe parameter script

--- Capstone-2025-2026/Assets/_Project/Editor/EditorTetherStripes.cs
+++ Capstone-2025-2026/Assets/_Project/Editor/EditorTetherStripes.cs
@@ -2,29 +2,29 @@ using Unity.Cinemachine;
 using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(TetherStripeParameters))]
+//[CustomEditor(typeof(TetherStripeParameters))]
 public class EditorTetherStripes : Editor
 {
-    private float stripeSize = 0.0f;
-    private float stripeAmount = 60.0f;
-    private float stripeScrollSpeed = -1.0f;
-    public override void OnInspectorGUI()
-    {
-        TetherStripeParameters selected = target as TetherStripeParameters;
+    //private float stripeSize = 0.0f;
+    //private float stripeAmount = 60.0f;
+    //private float stripeScrollSpeed = -1.0f;
+    //public override void OnInspectorGUI()
+    //{
+    //    TetherStripeParameters selected = target as TetherStripeParameters;
 
-        stripeSize = EditorGUILayout.Slider("Stripe Size", stripeSize, -1f, 1f);
-        stripeAmount = EditorGUILayout.Slider("Stripe Amount", stripeAmount, 1f, 50f);
-        stripeScrollSpeed = EditorGUILayout.Slider("Stripe Scroll Speed", stripeScrollSpeed, -5f, 5f);
+    //    stripeSize = EditorGUILayout.Slider("Stripe Size", stripeSize, -1f, 1f);
+    //    stripeAmount = EditorGUILayout.Slider("Stripe Amount", stripeAmount, 1f, 50f);
+    //    stripeScrollSpeed = EditorGUILayout.Slider("Stripe Scroll Speed", stripeScrollSpeed, -5f, 5f);
 
-        if(selected != null )
-        {
+    //    if(selected != null )
+    //    {
 
-            selected.SetStripeThickness(stripeSize);
-            selected.SetStripeCount(stripeAmount);
-            selected.SetStripeScrollSpeed(stripeScrollSpeed);
+    //        selected.SetStripeThickness(stripeSize);
+    //        selected.SetStripeCount(stripeAmount);
+    //        selected.SetStripeScrollSpeed(stripeScrollSpeed);
 
-            // Force scene to refresh
-            SceneView.RepaintAll();
-        }
-    }
+    //        // Force scene to refresh
+    //        SceneView.RepaintAll();
+    //    }
+    //}
 }
--- Capstone-2025-2026/Assets/_Project/Scripts/Player/JointTether/TetherStripeParameters.cs
+++ Capstone-2025-2026/Assets/_Project/Scripts/Player/JointTether/TetherStripeParameters.cs
@@ -1,8 +1,31 @@
 using UnityEditor;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class TetherStripeParameters : MonoBehaviour
 {
+    [SerializeField] float hiddenStripeScrollSpeed = -2f;
+    [SerializeField] float hiddenStripeThickness = 0f;
+    [SerializeField] float hiddenStripeLineAmount = 50f;
+    [ColorUsage(true, true)]
+    [SerializeField] Color inactiveEmmssive = Color.yellow;
+    [ColorUsage(true, true)]
+    [SerializeField] Color activeEmmissive = Color.green;
+    [SerializeField] MeshRenderer[] attachmentPoints;
+    private void Start()
+    {
+        SetStripeCount(hiddenStripeLineAmount);
+        SetStripeScrollSpeed(hiddenStripeScrollSpeed);
+        SetStripeThickness(hiddenStripeThickness);
+    }
+
+    private void OnValidate()
+    {
+        SetStripeCount(hiddenStripeLineAmount);
+        SetStripeScrollSpeed(hiddenStripeScrollSpeed);
+        SetStripeThickness(hiddenStripeThickness);
+    }
+
     public void SetStripeCount(float lineCount)
     {
         Shader.SetGlobalFloat("_HiddenStripeLineCount", lineCount);
