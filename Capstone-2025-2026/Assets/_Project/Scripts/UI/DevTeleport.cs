0999195e13ee0e2f11df46d0b15da47e516d9ad5
Drarist76 <deegankosen@gmail.com>
2025-11-21 17:23:42 -0500

    Added dev menu to quickly teleport and any other future dev stuff

---
+++ Capstone-2025-2026/Assets/_Project/Scripts/UI/DevTeleport.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DevTeleport : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    [SerializeField] private List<Transform> teleportLocations;
+    [SerializeField] private TMP_Dropdown teleportDropDown;
+    private int currentTeleportOption = 0;
+
+    private PlayerActions _actions;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _actions = _player.GetComponent<PlayerActions>();
+
+        teleportDropDown.ClearOptions();
+
+        List<string> teleportOptions = new List<string>();
+
+        foreach (Transform t in teleportLocations)
+        {
+            teleportOptions.Add(t.name);
+        }
+
+        teleportDropDown.AddOptions(teleportOptions);
+
+        teleportDropDown.onValueChanged.AddListener(OnDropDownValuesChanged);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_actions.ControlHeld)
+        {
+            if(_actions.RespawnDown)
+            {
+                TeleportToLocation();
+            }
+        }
+    }
+
+    private void OnDropDownValuesChanged(int index)
+    {
+        currentTeleportOption = index;
+        TeleportToLocation();
+    }
+
+    public void TeleportToLocation()
+    {
+        _player.transform.position = teleportLocations[currentTeleportOption].position;
+        _player.transform.rotation = teleportLocations[currentTeleportOption].rotation;
+        _player.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+    }
+}
