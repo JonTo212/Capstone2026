9575d2e00dd1ca77ec71b4bfa64ee210437b12a6
Braeden <123268086+BraedenHammill@users.noreply.github.com>
2025-12-11 13:58:56 -0500

    fixed object respawn, small level adjustments, coin pickup

--- Capstone-2025-2026/Assets/_Project/Scripts/BraedenCoin.cs
+++ Capstone-2025-2026/Assets/_Project/Scripts/BraedenCoin.cs
@@ -2,9 +2,38 @@ using UnityEngine;
 
 public class BraedenCoin : MonoBehaviour
 {
+    public float pickupDistance = 10f;
+    public float suctionSpeed = 50f;
+    private Transform player;
+    public AudioManager audioManager;
+
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
+        //playsound
+        audioManager.PlaySFX(audioManager.Collection, 1, 4f);
+
         //destroy
         Destroy(gameObject);
     }
+
+    private void Update()
+    {
+        //get distance to player
+        var distToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distToPlayer < pickupDistance)
+        {
+           transform.position = Vector3.MoveTowards(transform.position, player.position, Time.deltaTime * suctionSpeed);
+        }
+    }
+
 }
+
