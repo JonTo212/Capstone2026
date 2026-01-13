a53c129424486b519bdd6ebe5b698dd23178e197
Jonathan To <jonto398@gmail.com>
2026-01-12 21:25:02 -0500

    Added InputType detection to PlayerActions

--- Capstone-2025-2026/Assets/_Project/Scripts/Player/PlayerActions.cs
+++ Capstone-2025-2026/Assets/_Project/Scripts/Player/PlayerActions.cs
@@ -3,6 +3,12 @@ using UnityEngine.InputSystem;
 
 public class PlayerActions : MonoBehaviour
 {
+    public enum InputType
+    {
+        MouseKeyboard,
+        Controller
+    }
+
     public InputAction MoveAction { get; set; }
     private InputAction lookAction;
     private InputAction jumpAction;
@@ -26,6 +32,8 @@ public class PlayerActions : MonoBehaviour
     private InputAction controlAction;
     private InputAction devMenuAction;
     private InputAction respawnAction;
+
+    public InputType CurrentDevice { get; private set; }
 
     #region Public Accessors
     public Vector2 MoveInput => MoveAction.ReadValue<Vector2>();
@@ -145,11 +153,20 @@ public class PlayerActions : MonoBehaviour
         DisableAllInput();
     }
 
+    private void Update()
+    {
+        print(CurrentDevice);
+    }
+
     public void EnableAllInput()
     {
         foreach (var action in allActions)
         {
             if (action == null) continue;
+
+            action.started += OnInputReceived;
+            action.performed += OnInputReceived;
+
             action.Enable();
         }
     }
@@ -160,7 +177,23 @@ public class PlayerActions : MonoBehaviour
         {
             if (action == null) continue;
             if (action == ignoreAction) continue;
+
+            action.started -= OnInputReceived;
+            action.performed -= OnInputReceived;
+
             action.Disable();
+        }
+    }
+
+    private void OnInputReceived(InputAction.CallbackContext ctx)
+    {
+        if (ctx.control.device is Gamepad)
+        {
+            CurrentDevice = InputType.Controller;
+        }
+        else if (ctx.control.device is Mouse || ctx.control.device is Keyboard)
+        {
+            CurrentDevice = InputType.MouseKeyboard;
         }
     }
 
