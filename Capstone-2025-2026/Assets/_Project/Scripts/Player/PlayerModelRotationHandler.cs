25602c1c3b50eb8bea3eda11ae74c2077b34bd59
JonTo212 <117186718+JonTo212@users.noreply.github.com>
2026-01-21 17:42:48 -0500

    wip wall jumping

--- Capstone-2025-2026/Assets/_Project/Scripts/Player/PlayerModelRotationHandler.cs
+++ Capstone-2025-2026/Assets/_Project/Scripts/Player/PlayerModelRotationHandler.cs
@@ -1,11 +1,21 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerModelRotationHandler : MonoBehaviour
 {
+    public enum RotationState
+    {
+        Default,
+        WallJump
+    }
+
     [SerializeField] private GameObject playerObj;
     [SerializeField] private float rotationSpeed;
 
     private PlayerMovement _playerController;
+    private RotationState _currentRotationState;
+    private Coroutine _lockRotationCoroutine;
+    Quaternion desiredRot;
 
     void Awake()
     {
@@ -15,8 +25,38 @@ public class PlayerModelRotationHandler : MonoBehaviour
     void Update()
     {
         if (_playerController.WishDir == Vector3.zero) return;
+        if (_currentRotationState == RotationState.WallJump) return;
 
-        Quaternion desiredRot = Quaternion.LookRotation(_playerController.WishDir);
+        desiredRot = Quaternion.LookRotation(_playerController.WishDir);
         playerObj.transform.rotation = Quaternion.Slerp(playerObj.transform.rotation, desiredRot, rotationSpeed * Time.deltaTime);
+    }
+
+    public void SetNewRotationDir(Vector3 dir, float lockDuration)
+    {
+        desiredRot = Quaternion.LookRotation(dir);
+        if (_lockRotationCoroutine != null)
+        {
+            StopCoroutine(_lockRotationCoroutine);
+        }
+        _lockRotationCoroutine = StartCoroutine(RotationLockTimer(lockDuration));
+    }
+
+    private IEnumerator RotationLockTimer(float duration)
+    {
+        _currentRotationState = RotationState.WallJump;
+        _playerController.PlayerInput.ChangeSpecificInput("Move", false);
+
+        float lockTimer = 0;
+
+        while(lockTimer < duration)
+        {
+            playerObj.transform.rotation = Quaternion.Slerp(playerObj.transform.rotation, desiredRot, rotationSpeed * 3f * Time.deltaTime);
+
+            lockTimer += Time.deltaTime;
+            yield return null;
+        }
+
+        _playerController.PlayerInput.ChangeSpecificInput("Move", true);
+        _currentRotationState = RotationState.Default;
     }
 }
