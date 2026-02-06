f2adb3bd1db039faf4888ac2d161dd9153c07c5e
Braeden <123268086+BraedenHammill@users.noreply.github.com>
2026-02-06 15:17:20 -0500

    WIP launch star mechanic

---
+++ Capstone-2025-2026/Assets/_Project/Scripts/LaunchStar.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class LaunchStar : MonoBehaviour
+{
+    public EnvironmentalProp environmentalPropScript;
+    public Transform endPoint;
+    public float Speed = 2f;
+
+    public GameObject player;
+    public GameObject star;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (environmentalPropScript.IsSnared)
+        {
+            StartCoroutine(Launch());
+        }
+
+        print(Vector3.Distance(transform.position, endPoint.position));
+    }
+
+    IEnumerator Launch()
+    {
+
+        while (Vector3.Distance(transform.position, endPoint.position) > 0.1f)
+        {
+            //make player child of the star so it moves with it
+            player.transform.SetParent(star.transform);
+
+            star.transform.position = Vector3.MoveTowards(transform.position, endPoint.position, Speed * Time.deltaTime);
+            
+
+            yield return null;
+        }
+    }
+}
