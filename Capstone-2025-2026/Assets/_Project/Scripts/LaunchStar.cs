3221618ed88bb92a1d46a99744c77d8bd9de394d
Braeden <123268086+BraedenHammill@users.noreply.github.com>
2026-02-09 19:36:04 -0500

    platforming level segment

--- Capstone-2025-2026/Assets/_Project/Scripts/LaunchStar.cs
+++ Capstone-2025-2026/Assets/_Project/Scripts/LaunchStar.cs
@@ -3,42 +3,82 @@ using UnityEngine;
 
 public class LaunchStar : MonoBehaviour
 {
-    public EnvironmentalProp environmentalPropScript;
-    public Transform endPoint;
-    public float Speed = 2f;
+    //README THIS LAUNCH STAR SCRIPT IS USING SAME LOGIC AS ANIMAL RESPAWN FOR TESTING PURPOSES. WILL NEED TO UPDATE LATER
 
     public GameObject player;
-    public GameObject star;
+
+    //Components
+    private Rigidbody rb;
+
+    //respawning
+    public Transform spawnPosition;
+    public bool isFalling = false;
+    public float returnBuffer = 0.1f;
+
+    public float returnSpeed;// time in seconds it takes to reach destination
+    private Vector3 velocity; // need this for smoothdamp
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = player.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        if (environmentalPropScript.IsSnared)
+        if (other.CompareTag("Player"))
         {
-            StartCoroutine(Launch());
+            StartCoroutine(Respawn());
         }
-
-        print(Vector3.Distance(transform.position, endPoint.position));
     }
 
-    IEnumerator Launch()
+
+    IEnumerator Respawn()
     {
+        //Setup 
+        rb.isKinematic = true;
+        isFalling = true;
 
-        while (Vector3.Distance(transform.position, endPoint.position) > 0.1f)
+        //returnSpeedCurrent = 0;
+
+
+        // DELAY TIMER//
+        print("waiting");
+        //returnSpeedCurrent = 0;
+        yield return new WaitForSeconds(.5f);
+
+
+        // MOVE TOWARDS SPAWN POSITION //
+
+        //check if it is close enough to spawn position
+        var spawnDestination = new Vector3(spawnPosition.position.x, spawnPosition.position.y, spawnPosition.position.z);
+
+        while (Vector3.Distance(player.transform.position, spawnDestination) > returnBuffer)
         {
-            //make player child of the star so it moves with it
-            player.transform.SetParent(transform);
+            //accelerate overtime
+            //Mathf.Clamp(returnSpeedCurrent, 0, returnSpeedMax);
+            //returnSpeedCurrent += returnAcceleration * Time.deltaTime; //accelerate return speed over time
 
-            transform.position = Vector3.MoveTowards(transform.position, endPoint.position, Speed * Time.deltaTime);
-            
 
+            //move towards spawn position
+            player.transform.position = Vector3.SmoothDamp(player.transform.position, spawnDestination, ref velocity, returnSpeed);
+
+            // return when the result is null
             yield return null;
         }
+
+        // DELAY TIMER//
+        print("waiting");
+        yield return new WaitForSeconds(.5f);
+
+        //RESET//
+        print("reseting");
+
+        //Enable Components
+        rb.isKinematic = false;
+        isFalling = false;
+
+
     }
 }
