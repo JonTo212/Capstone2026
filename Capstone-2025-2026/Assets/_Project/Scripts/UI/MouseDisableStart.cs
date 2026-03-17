464f179d0a053644cd1ea770e87a6ac87d063bc8
TheOneSG <99147331+TheOneSG@users.noreply.github.com>
2026-03-17 14:40:34 -0400

    Mouse start fix

---
+++ Capstone-2025-2026/Assets/_Project/Scripts/UI/MouseDisableStart.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class MouseDisableStart : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Hides the cursor
+        Cursor.visible = false;
+    }
+}
