8868978374ea223238a48b6dc97c0a616b2ec7ee
eloravw <67245265+eloravw@users.noreply.github.com>
2026-03-17 11:43:01 -0400

    Video added

--- Capstone-2025-2026/Assets/_Project/Scripts/UI/CutsceneBehaviour.cs
+++ Capstone-2025-2026/Assets/_Project/Scripts/UI/CutsceneBehaviour.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.Video;
 
 public class CutsceneBehaviour : MonoBehaviour
 {
+    public VideoPlayer video;
+    public MySceneManager sceneManager;
+    private double videoLength;
+    private double elapsed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        videoLength = video.length - 4;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        elapsed += Time.deltaTime;
+
+        if (elapsed >= videoLength)
+        {
+            sceneManager.LoadNewScene(0);
+        }
     }
 }
